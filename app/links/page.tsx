6dc9b333e1a089b39ecd223f4492b2ca8978ca22
classic-k <88990430+classic-k@users.noreply.github.com>
2024-07-30 17:00:23 +0000

    update

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,4 +1,4 @@
-import { FaTwitter, FaInstagram, FaFacebook} from 'react-icons/fa';
+import { FaTwitter, FaInstagram, FaFacebook, FaEnvelope} from 'react-icons/fa';
 //import '../global.css'; // Assuming global styles are in global.css
 
 const SocialMediaLinks = () => {
@@ -6,18 +6,32 @@ const SocialMediaLinks = () => {
     { name: 'Twitter', url: 'https://x.com/regalmark', icon: <FaTwitter /> },
     { name: 'Instagram', url: 'https://instagram.com/regalmark', icon: <FaInstagram /> },
     { name: 'Facebook', url: 'https://facebook.com/regalmark', icon: <FaFacebook /> },
+    { name: 'Email', url: 'https://mailto@regalmark.com', icon: <FaEnvelope /> },
     // Add more social media as needed
   ];
 
   return (
     <div className="social-media-grid">
     
-      {socialMedia.map((media, index) => (
+      {socialMedia.map((media, index) => {
+        if(media.name === "Email")
+        {
+          return (
+          
+          <a key={index} href="#" className="social-media-link" target="_blank" rel="noopener noreferrer">
+            
+            <div className="social-media-icon">{media.icon}</div>
+            <span> {media.name}: {media.url} </span>
+          </a>)
+        }
+        else {
+      return  (
         <a key={index} href={media.url} className="social-media-link" target="_blank" rel="noopener noreferrer">
           <div className="social-media-icon">{media.icon}</div>
           <span>{media.name}</span>
         </a>
-      ))}
+      )
+}})}
     </div>
   );
 };
