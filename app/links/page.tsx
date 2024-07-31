1f47f6785e4e9b5e1aae3a69359d445895ca25f7
classic-k <88990430+classic-k@users.noreply.github.com>
2024-07-31 10:33:50 -0700

    Update page.tsx

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,16 +1,17 @@
-import { FaTwitter, FaInstagram, FaFacebook, FaEnvelope, FaWhatsapp} from 'react-icons/fa';
-//import '../global.css'; // Assuming global styles are in global.css
+import { FaTwitter, FaInstagram, FaFacebook, FaEnvelope, FaWhatsapp, FaTiktok} from 'react-icons/fa';
+
 
 const SocialMediaLinks = () => {
   const socialMedia = [
+    
     { name: 'Twitter', url: 'https://x.com/regalmark', icon: <FaTwitter /> },
-    { name: 'Instagram', url: 'https://instagram.com/regalmark', icon: <FaInstagram /> },
+    { name: 'Instagram', url: 'https://www.instagram.com/regalmark_official?igsh=YnJjN3I3Yjhkenl4 ', icon: <FaInstagram /> },
     { name: 'Facebook', url: 'https://facebook.com/regalmark', icon: <FaFacebook /> },
-     { name: 'WhatApss', url: 'https://api.whatsapp.com/send?phone=https://wa.me/qr/ugpwawux4q3la1', icon: <FaWhatsapp /> },
+     { name: 'WhatApps', url: 'https://api.whatsapp.com/send?phone=https://wa.me/qr/ugpwawux4q3la1', icon: <FaWhatsapp /> },
     { name: 'Email', url: 'mailto:olanrewaju.atobatele360@gmail.com', icon: <FaEnvelope /> },
-    // Add more social media as needed
+    { name: 'TikTok', url: 'https://www.tiktok.com/@regalmarkofficial?_t=8oTfDH6rEQg&_r=1', icon: <FaTiktok /> },
+    
   ];
-
   return (
     <div className="social-media-grid">
     
