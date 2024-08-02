3e690529d913fa4c8691ef7f335cfa050a052e70
classic-k <88990430+classic-k@users.noreply.github.com>
2024-08-02 09:09:51 -0700

    Update page.tsx

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -4,17 +4,16 @@ import { FaTwitter, FaInstagram, FaFacebook, FaEnvelope, FaWhatsapp, FaTiktok} f
 const SocialMediaLinks = () => {
   const socialMedia = [
     
-    { name: 'Twitter', url: 'https://x.com/regalmark', icon: <FaTwitter /> },
+    
     { name: 'Instagram', url: 'https://www.instagram.com/regalmark_official?igsh=YnJjN3I3Yjhkenl4 ', icon: <FaInstagram /> },
-    { name: 'Facebook', url: 'https://facebook.com/regalmark', icon: <FaFacebook /> },
-     { name: 'WhatApps', url: 'https://api.whatsapp.com/send?phone=https://wa.me/qr/ugpwawux4q3la1', icon: <FaWhatsapp /> },
-    { name: 'Email', url: 'mailto:olanrewaju.atobatele360@gmail.com', icon: <FaEnvelope /> },
+     { name: 'WhatsApp', url: 'https://api.whatsapp.com/send?phone=https://wa.me/qr/ugpwawux4q3la1', icon: <FaWhatsapp /> },
+    { name: 'Email', url: 'mailto:regalmark.info@gmail.com', icon: <FaEnvelope /> },
     { name: 'TikTok', url: 'https://www.tiktok.com/@regalmarkofficial?_t=8oTfDH6rEQg&_r=1', icon: <FaTiktok /> },
     
   ];
   return (
     <div className="social-media-grid">
-     <h2 className="social-media-title">Social Media Handles</h2>
+ 
       {socialMedia.map((media, index) => {
         if(media.name === "Email")
         {
