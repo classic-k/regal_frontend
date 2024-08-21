d3b8346d56039d9f775ef3a06883f1cb9911cb8e
classic-k <88990430+classic-k@users.noreply.github.com>
2024-08-21 08:06:13 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Logo from "@/public/logo.png"
 import "./globals.css";
-import Image from "next/image"
-import  Card from "@/components/card"
-//import Head from "next/head";
-//import Script from "next/script";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,8 +19,6 @@ export default function RootLayout({
     <html lang="en">
       
       <body className={inter.className}>
-        
-     <Card />
         {children}
       </body>
     </html>
